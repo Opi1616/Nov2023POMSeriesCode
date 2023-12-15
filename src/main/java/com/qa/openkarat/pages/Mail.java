1fa1929ca3e51f583b67442d2ac15fc9af267ce2
Opi1616 <naveeenselenium@gmail.com>
2023-12-15 13:57:14 +0530

    mail naveen

---
+++ src/main/java/com/qa/openkarat/pages/Mail.java
@@ -0,0 +1,14 @@
+package com.qa.openkarat.pages;
+
+public class Mail {
+
+	public void sendMail() {
+		System.out.println("Mail sent");
+
+	}
+
+	public void getMail() {
+		System.out.println("  get Mail ");
+	}
+
+}
